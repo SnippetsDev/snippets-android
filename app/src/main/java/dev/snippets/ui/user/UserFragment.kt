b97e67f510364c4d9790a2db9fbaa97c1436127b
Sanskar Agrawal <sanskar10100@gmail.com>
2021-11-16 00:37:28 +0530

    Add sample user info

--- app/src/main/java/dev/snippets/ui/user/UserFragment.kt
+++ app/src/main/java/dev/snippets/ui/user/UserFragment.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import coil.load
+import coil.transform.CircleCropTransformation
+import dev.snippets.R
 import dev.snippets.databinding.FragmentUserBinding
 
 class UserFragment : Fragment() {
@@ -22,6 +25,17 @@ class UserFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        with(binding) {
+            imageViewUserImage.load(R.drawable.sample_user_image) {
+                transformations(CircleCropTransformation())
+            }
 
+            textViewUserName.text = "Guido van Rossum"
+            textViewUserEmail.text = "guido@python.org"
+            textViewUserBio.text = "Creator of the Python programming language. " +
+                    "Holds a Master's degree in Computer Science and Mathematic. " +
+                    "Previously Senior Staff Engineer @ Google, Principal Engineer @ Dropbox. " +
+                    "Currently Distinguished Engineer @ Microsoft."
+        }
     }
 }
