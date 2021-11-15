97176deaa7bb7517d619d6adb4623ea1f5e66515
Sanskar Agrawal <sanskar10100@gmail.com>
2021-11-16 00:23:24 +0530

    Add bottom navigation controller

--- app/src/main/java/dev/snippets/ui/MainActivity.kt
+++ app/src/main/java/dev/snippets/ui/MainActivity.kt
@@ -2,11 +2,23 @@ package dev.snippets.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dev.snippets.R
+import dev.snippets.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNav.setupWithNavController(navController)
     }
 }
