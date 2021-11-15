4281f12af8750f2e1a8e8fe65f088bf3aaa4cce6
Sanskar Agrawal <sanskar10100@gmail.com>
2021-11-15 23:50:58 +0530

    Fixed LaunchActivity.kt package locatio

--- app/src/main/java/dev/snippets/ui/launch/LaunchActivity.kt
+++ app/src/main/java/dev/snippets/ui/launch/LaunchActivity.kt
@@ -1,4 +1,4 @@
-package dev.snippets.ui
+package dev.snippets.ui.launch
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import dev.snippets.R
+import dev.snippets.ui.MainActivity
 
 class LaunchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
