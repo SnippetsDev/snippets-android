409070a6cdfe3d801abd779f9234d9bb3dbb43db
Sanskar Agrawal <sanskar10100@gmail.com>
2022-03-27 02:27:10 +0530

    Add state holder and network error handler

--- app/src/main/java/dev/snippets/data/Repository.kt
+++ app/src/main/java/dev/snippets/data/Repository.kt
@@ -1,11 +1,40 @@
 package dev.snippets.data
 
+import dev.snippets.util.State
 import dev.snippets.util.log
+import retrofit2.HttpException
+import retrofit2.Response
 
 class Repository (
     private val api: Api
 ) {
     suspend fun getAllSnippets() {
         log(api.getAllSnippets().body().toString())
+    }
+
+    /**
+     * Takes in an api call and returns an appropriate UI state based on the response
+     */
+    private suspend fun <T> dataOrError(apiCall: suspend () -> Response<T>): State<T> {
+        try {
+            val response = apiCall()
+            return when {
+                response.code() != 200 -> {
+                    State.Error(response.message())
+                }
+                else -> {
+                    State.Success(response.body()!!)
+                }
+            }
+        } catch (exception: Exception) {
+            log("Caught exception while making API call, Exception: $exception, Message: ${exception.message}")
+            val errorPrompt = if (exception is HttpException) {
+                exception.message ?: "Something went wrong"
+            } else {
+//                Firebase.crashlytics.recordException(exception)
+                "Something went wrong"
+            }
+            return State.Error(errorPrompt)
+        }
     }
 }
---
+++ app/src/main/java/dev/snippets/util/State.kt
@@ -0,0 +1,10 @@
+package dev.snippets.util
+
+/**
+ * Represents the state of the UI at a given point in time.
+ */
+sealed class State<out T> {
+    object Loading: State<Nothing>()
+    class Success<T>(val data: T): State<T>()
+    class Error(val message: String): State<Nothing>()
+}
