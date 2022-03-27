b349fac00b6ddede2ba730ac026032695771dc33
Sanskar Agrawal <sanskar10100@gmail.com>
2022-03-27 13:40:26 +0530

    Update snippet data model to hold imageUrl and code

--- app/src/main/java/dev/snippets/data/Snippet.kt
+++ app/src/main/java/dev/snippets/data/Snippet.kt
@@ -1,5 +1,7 @@
 package dev.snippets.data
 
+import com.squareup.moshi.Json
+
 data class SnippetsApiResponse(
     val error: Any?,
     val snippets: List<Snippet>,
@@ -11,5 +13,7 @@ data class Snippet(
     val title: String,
     val language: String,
     val description: String,
-    val tags: List<String>
+    val tags: List<String>,
+    @Json(name = "img_url") val imageUrl: String,
+    val code: String
 )
