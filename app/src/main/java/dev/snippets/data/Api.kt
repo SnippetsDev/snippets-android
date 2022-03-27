91ee9c25f5ef38c382036b0f5028cae46181a5e0
Sanskar Agrawal <sanskar10100@gmail.com>
2022-03-28 01:26:16 +0530

    Update detail fragment to render actual data on click

--- app/src/main/java/dev/snippets/data/Api.kt
+++ app/src/main/java/dev/snippets/data/Api.kt
@@ -1,14 +1,15 @@
 package dev.snippets.data
 
 import retrofit2.Response
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.POST
+import retrofit2.http.*
 
 interface Api {
 
     @GET("/snippets")
     suspend fun getAllSnippets(): Response<SnippetsApiResponse>
+
+    @GET("/snippets")
+    suspend fun getSnippet(@Query("id") id: String): Response<SnippetsApiResponse>
 
     @GET("/tags")
     suspend fun getAllTags(): Response<TagsApiResponse>
