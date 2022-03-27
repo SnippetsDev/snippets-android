c82b6074fb2f39b50a6d5181b908ad742bad895d
Sanskar Agrawal <sanskar10100@gmail.com>
2022-03-27 21:06:30 +0530

    Add konfetti, and remove back stack pop on snippet publish

--- app/src/main/java/dev/snippets/util/Constants.kt
+++ app/src/main/java/dev/snippets/util/Constants.kt
@@ -19,4 +19,6 @@ object Constants {
 
     const val KEY_INPUT_CODE_DIALOG = "key_input_code_dialog"
     const val KEY_CODE = "key_code"
+
+    const val KEY_PUBLISHED_FIRST_SNIPPET = "key_published_first_snippet"
 }
