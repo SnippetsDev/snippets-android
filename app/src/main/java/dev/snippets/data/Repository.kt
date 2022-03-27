585b2a628b50a9658a74296926dc31a923683e3d
Sanskar Agrawal <sanskar10100@gmail.com>
2022-03-27 20:35:42 +0530

    Add image upload and error handling

--- app/src/main/java/dev/snippets/data/Repository.kt
+++ app/src/main/java/dev/snippets/data/Repository.kt
@@ -1,11 +1,19 @@
 package dev.snippets.data
 
+import android.net.Uri
+import androidx.lifecycle.liveData
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import dev.snippets.util.State
+import dev.snippets.util.getUniqueNameForImage
 import dev.snippets.util.log
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 import retrofit2.HttpException
 import retrofit2.Response
+import kotlin.coroutines.coroutineContext
 
 class Repository (
     private val api: Api
@@ -13,6 +21,25 @@ class Repository (
     suspend fun getAllSnippets() = dataOrError { api.getAllSnippets() }
 
     suspend fun getAllTags() = dataOrError { api.getAllTags() }
+
+    fun uploadImageToFirebase(imageUri: Uri) = liveData {
+        emit(State.Loading)
+        val ref = Firebase.storage.reference.child("snippet_outputs/${getUniqueNameForImage()}")
+        val uploadTask = ref.putFile(imageUri)
+        try {
+            val result = uploadTask.continueWithTask { task ->
+                if (!task.isSuccessful) {
+                    task.exception?.let {
+                        throw it
+                    }
+                }
+                ref.downloadUrl
+            }.await()
+            emit(State.Success(result.toString()))
+        } catch (e: Exception) {
+            emit(State.Error("Failed to upload image, ${e.message}"))
+        }
+    }
 
     /**
      * This is a really common pattern where the API call result needs to be checked for errors.
