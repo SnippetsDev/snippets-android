742c9f2493f841fc90b3e43d3a7157c389bac644
Sanskar Agrawal <sanskar10100@gmail.com>
2022-03-27 16:59:58 +0530

    Add image picking

--- app/src/main/java/dev/snippets/ui/create/CreateFragment.kt
+++ app/src/main/java/dev/snippets/ui/create/CreateFragment.kt
@@ -1,19 +1,27 @@
 package dev.snippets.ui.create
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.github.dhaval2404.imagepicker.ImagePicker
 import dagger.hilt.android.AndroidEntryPoint
 import dev.snippets.databinding.FragmentCreateBinding
 import dev.snippets.util.Constants
+import dev.snippets.util.errorSnackbar
+import dev.snippets.util.log
 
 @AndroidEntryPoint
 class CreateFragment : Fragment() {
     private lateinit var binding: FragmentCreateBinding
+    private val model by viewModels<CreateViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,10 +35,33 @@ class CreateFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setupViews()
+    }
+
+    private fun setupViews() {
         with (binding) {
             buttonInputCode.setOnClickListener {
                 InputCodeDialogFragment().show(parentFragmentManager, "INPUT_MODAL")
             }
+
+            imageViewPickCamera.setOnClickListener {
+                ImagePicker.with(this@CreateFragment)
+                    .cameraOnly()
+                    .crop()
+                    .compress(1024)
+                    .maxResultSize(1080, 1080)
+                    .createIntent { startForImageResult.launch(it) }
+            }
+
+            imageViewPickGallery.setOnClickListener {
+                ImagePicker.with(this@CreateFragment)
+                    .galleryOnly()
+                    .crop()
+                    .compress(1024)
+                    .maxResultSize(1080, 1080)
+                    .createIntent { startForImageResult.launch(it) }
+            }
+
 
             (textFieldLanguage.editText as? AutoCompleteTextView)?.setAdapter(
                 ArrayAdapter(
@@ -39,6 +70,19 @@ class CreateFragment : Fragment() {
                     Constants.listOfLanguages
                 )
             )
+        }
+    }
+
+    private val startForImageResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        val resultCode = result.resultCode
+        val data = result.data
+
+        if (resultCode == Activity.RESULT_OK) {
+            //Image Uri will not be null for RESULT_OK
+            model.imageUri = data?.data!!
+            log("Received uri: ${model.imageUri}")
+        } else if (resultCode == ImagePicker.RESULT_ERROR) {
+            binding.root.errorSnackbar(ImagePicker.getError(data))
         }
     }
 }
--- app/src/main/java/dev/snippets/ui/create/CreateViewModel.kt
+++ app/src/main/java/dev/snippets/ui/create/CreateViewModel.kt
@@ -1,5 +1,7 @@
 package dev.snippets.ui.create
 
+import android.net.Uri
+import androidx.core.net.toUri
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.snippets.data.Repository
@@ -10,4 +12,5 @@ class CreateViewModel @Inject constructor(
     private val repo: Repository
 ) : ViewModel() {
 
+    var imageUri = "".toUri()
 }
